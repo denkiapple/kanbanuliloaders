65d742eff35e60a693b543dd1defd0bd1c374106
Ulises Torner <utorner@gmail.com>
2020-08-30 00:11:30 -0500

    jk

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { dolorSit } from "./constants";
 import { ColumnLoader } from ".";
 import styles from "./Kanban.module.css";
