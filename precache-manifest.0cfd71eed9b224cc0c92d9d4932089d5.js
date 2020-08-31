2c6a1eab7eca34e1a0133c9fd5c0435afff0138a
Ulises Torner <utorner@gmail.com>
2020-08-31 10:37:40 -0500

    Updates

---
+++ precache-manifest.0cfd71eed9b224cc0c92d9d4932089d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ffec2ebb0d1146b3a0ede7dccd5d7d",
+    "url": "/kanbanuliloaders/index.html"
+  },
+  {
+    "revision": "cbf1b9545a53f1b37243",
+    "url": "/kanbanuliloaders/static/css/main.da1ed5c4.chunk.css"
+  },
+  {
+    "revision": "085245b5105ad3c5acd2",
+    "url": "/kanbanuliloaders/static/js/2.6a3d70f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanbanuliloaders/static/js/2.6a3d70f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf1b9545a53f1b37243",
+    "url": "/kanbanuliloaders/static/js/main.75d80dbf.chunk.js"
+  },
+  {
+    "revision": "bb14bb5141f9d5191ffe",
+    "url": "/kanbanuliloaders/static/js/runtime-main.4c8689ff.js"
+  }
+]);
